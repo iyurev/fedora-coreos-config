1684eb390d6c77805cf0f940c3f90704515e4145
Igor <idyurev@gmail.com>
2019-11-22 14:10:35 +0300

    Fix missing kubernetes include

--- test-ignitions/start-vm.sh
+++ test-ignitions/start-vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-${VM_DISK_PATH}=$1
-${PATH_TO_IGN}=$2
+VM_DISK_PATH=$1
+PATH_TO_IGN=$2
 
 qemu-system-x86_64 -machine accel=kvm -m 2048 -cpu host -nographic \
 	-drive if=virtio,file=${VM_DISK_PATH} \
